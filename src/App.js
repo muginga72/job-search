45cc32f4aa14c4aa53ce755e1d6902dfbb47f796
Muginga <74222775+muginga72@users.noreply.github.com>
2021-10-25 21:38:53 -0400

    My application for job search.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const jobs = [ 
+  {
+    jobID: 9901,
+    title: "Developer",
+    url: "https://hiring.amazon.com/",
+    company: "Amazon",
+    position: "Junior-Dev",
+    date:"10/23/2021",
+    jobOffer: "No"
+  },
+  
+  {
+    jobID: 1902,
+    title: "Software Eng.",
+    url: "https://careers.google.com/jobs/",
+    company: "Google",
+    position: "Softwares Eng.",
+    date:"9/23/2021",
+    jobOffer: "No",
+  }
+]
+  
+const App = () => {
+  return(
+    <div>
+      <h1>My Search for Job</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      
+      <hr />
+      
+      <ul>
+        {jobs.map(function (item) {
+          return (
+            <li key={ item.jobID }>
+              <span>
+                <a href={item.url}>{ item.title }                 </a>
+              </span>
+              <span> { item.company } </span>
+              <span> <strong>{ item.position } </strong></span>
+              <span> { item.date } </span>
+              <span> { item.jobOffer } </span>
+            </li>
+          );
+        })}
+        
+      </ul>
+      
+   </div>
   );
 }
 
